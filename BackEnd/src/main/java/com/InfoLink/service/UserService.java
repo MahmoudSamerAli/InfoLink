b3d3d5903d8b4cd292c18909e6f2e8618ddda9bc
MahmoudSamerAli <mahmoud.samer2005@gmail.com>
2026-08-20 11:20:27 +0300

    Logic Updates

--- BackEnd/src/main/java/com/InfoLink/service/UserService.java
+++ BackEnd/src/main/java/com/InfoLink/service/UserService.java
@@ -79,8 +79,8 @@ public class UserService {
         newUser.setIsActive(true);
         return userRepository.save(newUser);
     }
-    public User updateUser(PatchUserRequest request) {
-        return userRepository.findById(request.getUserID())
+    public User updateUser(PatchUserRequest request, int id) {
+        return userRepository.findById(id)
             .map(user -> {
                 if (request.getUsername() != null) {
                     user.setUsername(request.getUsername().trim());
@@ -104,7 +104,7 @@ public class UserService {
                 }
                 return userRepository.save(user);
         })
-        .orElseThrow(() -> new RuntimeException("User not found with id: " + request.getUserID()));
+        .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
     }
     
     public void deleteUser(int id) {
