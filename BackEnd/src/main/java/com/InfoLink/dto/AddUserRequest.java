f7a29cc46f61baa66a764031191e9e26d52782a8
HassanMohamed-cs <hassanashour639@gmail.com>
2026-08-23 21:45:18 +0300

    Minor change

--- BackEnd/src/main/java/com/InfoLink/dto/AddUserRequest.java
+++ BackEnd/src/main/java/com/InfoLink/dto/AddUserRequest.java
@@ -10,7 +10,7 @@ import jakarta.validation.constraints.Size;
 public class AddUserRequest {
     @NotBlank
     @Size(min = 4, max = 30, message = "Username must be between 4 and 30 characters")
-    @Pattern(regexp = "^[a-zA-Z0-9_\\.]+$", message = "Username can only contain letters, numbers, and underscores")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain letters, numbers, and underscores")
     private String username;
 
     @NotBlank
@@ -34,6 +34,7 @@ public class AddUserRequest {
 
     @NotNull(message = "isActive flag is required")
     private Boolean isActive;
+
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
